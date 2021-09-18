f622c3eaaa64ff35eefa9763ca8e9ec288172442
Braeden Mulligan <braeden.mulligan@gmail.com>
2021-09-18 09:01:41 -0700

    Create debug app

--- src/main.c
+++ src/main.c
@@ -1,7 +1,5 @@
-//#include "ESP8266_link.h"
 #include "arduino_wifi_app.h"
-//#include "avr_timer_util.h"
-//#include "avr_utilities.h"
+#include "avr_utilities.h"
 
 #include <stdio.h>
 #include <string.h>
@@ -12,7 +10,16 @@
 char server_message[SERVER_MSG_SIZE_MAX];
 
 void handle_server_message(void) {
+	blink_led(5, 250);
+}
 
+uint16_t debug_counter = 0;
+
+void app_loop(void) {
+	char msg[16];
+	sprintf(msg, "dbg: %u", debug_counter);
+	wifi_send(msg);
+	debug_counter += 1;
 }
 
 int main(void) {
@@ -20,35 +27,18 @@ int main(void) {
 	struct wifi_app_config app_conf = wifi_app_config_create();
 
 	app_conf.wifi_startup_timeout = 10;
-	app_conf.module_check_interval = 60;
+	app_conf.application_interval = 5;
 	app_conf.server_buf_size = SERVER_MSG_SIZE_MAX;
 	app_conf.server_message_buf = server_message;
 	app_conf.server_message_callback = handle_server_message;
-	app_conf.app_main_callback = NULL;
+	app_conf.app_main_callback = app_loop;
 	
 	wifi_app_init(&app_conf);
 
 	wifi_app_start();
 
-	/*
-	//module_startup_procedure();
-	uint16_t debug_count = 0;
-
-	timer16_init(5);
-	timer16_start();
-
-	for (ever) {
+	// If here is reached error occurred
+	blink_led(-1, 1000);
 	
-		if (timer16_flag) {
-				sprintf(debug_msg, "Sameple message #%d\r\n", debug_count);
-				ESP8266_socket_send(&esp_params, 3000, debug_msg, strlen(debug_msg));
-				++debug_count;
-			}
-
-			timer16_reset();
-		}
-	}
-*/
-
 	return 0;
 }

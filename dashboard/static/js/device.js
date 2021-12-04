91287f983fa502b5a3c18be41abcc5cd6cbd3d6b
Braeden Mulligan <braeden.mulligan@gmail.com>
2021-12-03 22:33:46 -0800

    Improve DOM manipulation

--- dashboard/static/js/device.js
+++ dashboard/static/js/device.js
@@ -1,49 +1,52 @@
 class SH_Device {
-	static SH_TYPE_RESERVED_1 = 0;
-	static SH_TYPE_RESERVED_2 = 1;
-	static SH_TYPE_RESERVED_3 = 2;
-	static SH_TYPE_RESERVED_4 = 3;
-	static SH_TYPE_RESERVED_5 = 4;
-	static SH_TYPE_CAMERA = 5;
+	static SH_TYPE_NULL = 0;
+	static SH_TYPE_RESERVED_1 = 1;
+	static SH_TYPE_RESERVED_2 = 2;
+	static SH_TYPE_RESERVED_3 = 3;
+	static SH_TYPE_RESERVED_4 = 4;
+	static SH_TYPE_RESERVED_5 = 5;
 	static SH_TYPE_IRRIGATION = 6;
 	static SH_TYPE_POWEROUTLET = 7;
 	static SH_TYPE_THERMOSTAT = 8;
+	static SH_TYPE_CAMERA = 9;
 
-	static GENERIC_REG_NULL = 0;
-	static GENERIC_REG_RESERVED_1 = 1;
-	static GENERIC_REG_RESERVED_2 = 2;
-	static GENERIC_REG_RESERVED_3 = 3;
-	static GENERIC_REG_RESERVED_4 = 4;
-	static GENERIC_REG_RESERVED_5 = 5;
-	static GENERIC_REG_ENABLE = 6;
-	static GENERIC_REG_PING = 7;
-	static GENERIC_REG_DATE = 9;
-	static GENERIC_REG_TIME = 10;
-	static GENERIC_REG_SCHEDULE = 12;
-	static GENERIC_REG_SCHEDULE_ENABLE = 13;
-	static GENERIC_REG_SCHEDULE_COUNT = 14;
-	static GENERIC_REG_APP_FREQUENCY = 20;
-	static GENERIC_REG_POLL_FREQUENCY = 21;
-	static GENERIC_REG_PUSH_ENABLE = 31;
-	static GENERIC_REG_PUSH_FREQUENCY = 32;
-	static GENERIC_REG_PUSH_BUFFERING = 33;
+	static elem_tag_id = "div";
+	static elem_tag_name = "div";
+	static elem_tag_online = "div";
+		
+	static class_label_id = "sh-device-attr-id";
+	static class_label_name = "sh-device-attr-name";
+	static class_label_online = "sh-device-attr-online";
 
-	static CMD_NUL = 0;
-	static CMD_GET = 1;
-	static CMD_SET = 2;
+	schedules = [];
 
-	static format_message(cmd, reg, val) {
-		var message = cmd.toString(16).toUpperCase().padStart(2, "0") + ",";
-		message += reg.toString(16).toUpperCase().padStart(2, "0") + ",";
-		message += val.toString(16).toUpperCase().padStart(8, "0");
-		return message;
-	}
-
-	constructor(type, id, name, online, registers) {
+	constructor(type, id, name, online) {
 		this.type = type;
 		this.id = id;
 		this.name = name;
-		this.online = online
-		this.registers = {}
+		this.online = online;
+	}
+
+	write_html() {
+		var device_elem = document.createElement("div");
+		device_elem.setAttribute("id", "device-" + this.id.toString());
+		device_elem.setAttribute("class", "sh-device"); 
+
+		var attr = document.createElement(SH_Device.elem_tag_id);
+		attr.innerHTML = this.id.toString();
+		attr.setAttribute("class", SH_Device.class_label_id);
+		device_elem.append(attr);
+
+		attr = document.createElement(SH_Device.elem_tag_name);
+		attr.innerHTML = this.name.toString();
+		attr.setAttribute("class", SH_Device.class_label_name); 
+		device_elem.append(attr);
+
+		attr = document.createElement(SH_Device.elem_tag_online);
+		attr.innerHTML = this.online.toString();
+		attr.setAttribute("class", SH_Device.class_label_online); 
+		device_elem.append(attr);
+
+		return device_elem;
 	}
 }
--- dashboard/static/js/poweroutlet.js
+++ dashboard/static/js/poweroutlet.js
@@ -1,71 +1,94 @@
 // TODO: unhardcode this stuff
 let server_addr = "192.168.1.79";
 
-class Poweroutlet extends SH_Device {
-	static get_count() {
-		return this.format_message(this.CMD_GET, this.POWEROUTLET_REG_OUTLET_COUNT, 0);
-	}
-
-	static get_state() {
-		return this.format_message(this.CMD_SET, this.POWEROUTLET_REG_STATE, 0);
-	}
-
-	// pass a list of tuples (<outlet index>, <boolean value>)
-	static set_state(outlet_values) {
-		var high_byte = 0;
-		var low_byte = 0;
-
-		for (var i = 0; i < outlet_values.length; ++i) {
-			var [index, val] = outlet_values[i];
-
-			high_byte |= (1 << index);
-			if (val) low_byte |= (1 << index);
-		}
-
-		var reg_value = high_byte << 8 | low_byte;
-
-		return this.format_message(this.CMD_SET, this.POWEROUTLET_REG_STATE, reg_value);
-	}
-
-	// return list of tuples (<outlet index>, <boolean value>)
-	static parse_state(reg_value, outlet_count = 8) {
-		if (outlet_count > 8) {
-			console.log("WARNING: poweroutlet_read_state invalid argument passed.");
-			outlet_count = 8;
-		}
-
-		if (typeof(reg_value) === "string") {
-			reg_value = parseInt(reg_value, 16);
-		} else if (typeof(reg_value !== "number")) {
-			console.log("WARNING: in parse_state(), invalid reg_value");
-		}
-	
-		var outlet_values = [];
-
-		for (var i = 0; i < outlet_count; ++i) {
-			if (reg_value & (1 << i)) {
-				outlet_values.push([i, true]);
-			} else {
-				outlet_values.push([i, false]);
-			}
-		}
-
-		return outlet_values;
-	}
-	
-	constructor(id, name, online, registers) {
-		SH_Device.POWEROUTLET_REG_STATE = 101;
-		SH_Device.POWEROUTLET_REG_OUTLET_COUNT = 102;
-		super(SH_Device.SH_TYPE_POWEROUTLET, id, name, online, registers);
-	}
+function toggle_action(evt) {
+	console.log("Clicked device " + evt.target.device_id);
+	//send request
+	//track changes
 }
 
-device = new Poweroutlet(0, "None", false, {});
+class Poweroutlet extends SH_Device {
+	static elem_tag_socket_count = "div";
 
-var state_cmd = Poweroutlet.set_state([[0,1],[2,1],[1,0],[3,0]]);
-console.log(state_cmd);
-console.log(Poweroutlet.parse_state(state_cmd.split(',')[2]));
+	static class_label_socket_count = "poweroutlet-attr-socket-count";
+	static class_label_socket_list = "poweroutlet-attr-socket-list";
+	static class_label_socket_item = "poweroutlet-attr-socket-item";
+	static class_label_socket_state = "poweroutlet-attr-socket-state";
 
+	constructor(id, name, online, socket_count = 0, socket_states = []) {
+		//SH_Device.POWEROUTLET_REG_STATE = 101;
+		//SH_Device.POWEROUTLET_REG_OUTLET_COUNT = 102;
+		super(SH_Device.SH_TYPE_POWEROUTLET, id, name, online);
+		this.socket_count = socket_count;
+		this.socket_states = socket_states;
+	}
+
+	write_html() {
+		var device_elem = document.getElementById("device-" + this.id.toString());
+
+		if (device_elem != null) {
+			// Update all attributes that are mutable.
+			var socket_list;
+
+			for (var i = 0; i < device_elem.children.length; ++i) {
+				var attr = device_elem.children[i];
+				if (attr.className == SH_Device.class_label_name) {
+					attr.innerHTML = this.name;
+
+				} else if (attr.className == SH_Device.class_label_online) {
+					attr.innerHTML = this.online.toString();
+
+				} else if (attr.className == Poweroutlet.class_label_socket_list) {
+					socket_list = attr;
+				}
+			}
+
+			//each item should have a button and a pair/value
+			for (var i = 0; i < socket_list.children.length; ++i) {
+				var socket_item = socket_list.children[i];
+				for (var j = 0; j < socket_item.children.length; ++j) {
+					if (socket_item.children[j].className == Poweroutlet.class_label_socket_state) {
+//TODO: properly associate object index with labeled index
+						socket_item.children[j].innerHTML = "Socket " + this.socket_states[i][0] + ": " + (this.socket_states[i][1] ? "ON" : "OFF");
+					}
+				}
+			}
+	
+			return null;
+		}
+
+		device_elem = super.write_html();
+
+		var p = document.createElement(Poweroutlet.elem_tag_socket_count);
+		p.innerHTML = "Socket count: " + this.socket_count;
+		device_elem.append(p);
+
+		var outlet_list = document.createElement("ul");
+		outlet_list.setAttribute("class", Poweroutlet.class_label_socket_list);
+		for (var i = 0; i < this.socket_states.length; ++i) {
+			var entry = document.createElement("li");
+			entry.setAttribute("class", Poweroutlet.class_label_socket_item);
+
+			var outlet_pair = document.createElement("p");
+			outlet_pair.setAttribute("class", Poweroutlet.class_label_socket_state);
+			outlet_pair.innerHTML = "Socket " + this.socket_states[i][0] + ": " + (this.socket_states[i][1] ? "ON" : "OFF");
+
+			var toggle_button =  document.createElement("button");
+			toggle_button.innerHTML = "Toggle";
+			toggle_button.device_id = this.id;
+			toggle_button.addEventListener('click', toggle_action, false);
+
+			entry.append(outlet_pair);
+			entry.append(toggle_button);
+
+			outlet_list.append(entry);
+		}
+
+		device_elem.append(outlet_list);
+
+		return device_elem;
+	}
+}
 
 function refresh_devices() {
 	console.log("fetching devices");
@@ -80,9 +103,9 @@ function refresh_devices() {
 	}
 
 	xhr.onreadystatechange = function() {
-	if (xhr.readyState == XMLHttpRequest.DONE) {
-		console.log(xhr.responseText);
-		poweroutlets = JSON.parse(xhr.responseText);
+		if (xhr.readyState == XMLHttpRequest.DONE) {
+			console.log(xhr.responseText);
+			poweroutlets = JSON.parse(xhr.responseText);
 		}
 	}
 
@@ -90,7 +113,30 @@ function refresh_devices() {
 	xhr.send();
 }
 
-function display_device(device_obj) {
-	//get by id device panel
-	//id, type, state register
+var timeout_handle;
+var interval_handle;
+
+devices = [];
+
+// Test ----
+devices.push(new Poweroutlet(69, "Nice device", false, 2, [[0,1],[1,1]]));
+devices.push(new Poweroutlet(420, "Dank device", false, 3, [[0,1],[1,0],[2,1]]));
+
+const device_panel = document.getElementById("device-panel");
+
+for (var i = 0; i < devices.length; ++i) {
+	device_node = devices[i].write_html();
+	console.log(device_node);
+	device_panel.append(device_node);
+	device_panel.append(document.createElement("br"));
 }
+
+setTimeout(function() {
+	console.log("Updated devices");
+	devices[0].online = true;
+	devices[0].socket_states[0][1] = false;
+	devices[0].write_html();
+}, 2000);
+
+
+

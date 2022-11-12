be0e813850f652aef5ce5a9f3d624aad7b313363
Braeden Mulligan <braeden.mulligan@gmail.com>
2022-11-11 16:04:57 -0800

    Finish tictactoe implementation

--- server/dashboard/client-src/tictac.js
+++ server/dashboard/client-src/tictac.js
@@ -1,3 +1,25 @@
+function calculate_winner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+}
+
 //class Square extends React.Component {
 //  render() {
 function Square(props) {
@@ -10,60 +32,107 @@ function Square(props) {
 //}
 
 class Board extends React.Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			squares: Array(9).fill(null)
-		}
-	}
-
-	handle_click(i) {
-		console.log("clicked " + i);
-		const squares = this.state.squares.slice();
-		squares[i] = 'X';
-		this.setState({squares: squares});
-	}
 
   renderSquare(i) {
-    return <Square value={this.state.squares[i]} click_back={ () => this.handle_click(i) } />;
+    return <Square value={this.props.squares[i]} click_back={ () => this.props.click_back(i) } />;
   }
 
   render() {
-    const status = 'Next player: X';
+	let table = []
+	let row = [];
+
+	for (let i = 1; i < 10; ++i) {
+		row.push(this.renderSquare(i - 1));
+		if (i % 3 == 0) {
+			let x = <div className="board-row">{row}</div>
+			table.push(x);
+			row = []
+		}
+	}
 
     return (
       <div>
-        <div className="status">{status}</div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
+		{table}
       </div>
     );
   }
 }
 
 class Game extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			history: [
+				{ squares: Array(9).fill(null) }
+			],
+			step: 0,
+			x_next: true,
+		}
+	}
+
+  jump_to(step) {
+    this.setState({
+      step: step,
+      x_next: (step % 2) === 0,
+    });
+  }
+
+	handle_click(i) {
+		const history = this.state.history.slice(0, this.state.step + 1);
+		const current = history[history.length - 1];
+		const squares = current.squares.slice();
+
+		if (calculate_winner(squares) || squares[i]) return;
+
+		squares[i] = this.state.x_next ? 'X' : 'O';
+		this.setState({
+			history: history.concat([{squares: squares}]),
+			step: history.length,
+			x_next: !this.state.x_next,
+		});
+	}
+
+	reset_board() {
+		this.setState({
+			history : [
+				{ squares: Array(9).fill(null) }
+			],
+			step: 0,
+			x_next: true,
+		})
+	}
+
   render() {
+	let history = this.state.history;
+    let current = history[this.state.step];
+	let winner = calculate_winner(current);
+
+	const moves = history.map((step, move) => {
+      const desc = move ? 'Go to move #' + move : 'Go to game start';
+      return (
+        <li key={move}>
+          <button onClick={() => this.jump_to(move)}>{desc}</button>
+        </li>
+      );
+    });
+
+    let status;
+	if (winner) {
+		status = "Winner: " + winner;
+	} else {
+		status = 'Next turn: ' + (this.state.x_next ? 'X' : 'O');
+	}
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          <Board squares={ current.squares } click_back={ (i) => this.handle_click(i) } />
         </div>
+
         <div className="game-info">
-          <div>{/* status */}</div>
-          <ol>{/* TODO */}</ol>
+          <div>{ status }</div>
+          <ol>{ moves }</ol>
+			<button className="reset" onClick={ () => this.reset_board() } > Reset </button>
         </div>
       </div>
     );
--- server/dashboard/static/js/tictac.js
+++ server/dashboard/static/js/tictac.js
@@ -1,10 +1,29 @@
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
+var _slicedToArray = function () { function sliceIterator(arr, i) { var _arr = []; var _n = true; var _d = false; var _e = undefined; try { for (var _i = arr[Symbol.iterator](), _s; !(_n = (_s = _i.next()).done); _n = true) { _arr.push(_s.value); if (i && _arr.length === i) break; } } catch (err) { _d = true; _e = err; } finally { try { if (!_n && _i["return"]) _i["return"](); } finally { if (_d) throw _e; } } return _arr; } return function (arr, i) { if (Array.isArray(arr)) { return arr; } else if (Symbol.iterator in Object(arr)) { return sliceIterator(arr, i); } else { throw new TypeError("Invalid attempt to destructure non-iterable instance"); } }; }();
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
+
+function calculate_winner(squares) {
+  var lines = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8], [0, 4, 8], [2, 4, 6]];
+
+  for (var i = 0; i < lines.length; i++) {
+    var _lines$i = _slicedToArray(lines[i], 3),
+        a = _lines$i[0],
+        b = _lines$i[1],
+        c = _lines$i[2];
+
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+
+  return null;
+}
 
 //class Square extends React.Component {
 //  render() {
@@ -22,68 +41,44 @@ function Square(props) {
 var Board = function (_React$Component) {
   _inherits(Board, _React$Component);
 
-  function Board(props) {
+  function Board() {
     _classCallCheck(this, Board);
 
-    var _this = _possibleConstructorReturn(this, (Board.__proto__ || Object.getPrototypeOf(Board)).call(this, props));
-
-    _this.state = {
-      squares: Array(9).fill(null)
-    };
-    return _this;
+    return _possibleConstructorReturn(this, (Board.__proto__ || Object.getPrototypeOf(Board)).apply(this, arguments));
   }
 
   _createClass(Board, [{
-    key: "handle_click",
-    value: function handle_click(i) {
-      console.log("clicked " + i);
-      var squares = this.state.squares.slice();
-      squares[i] = 'X';
-      this.setState({ squares: squares });
-    }
-  }, {
     key: "renderSquare",
     value: function renderSquare(i) {
       var _this2 = this;
 
-      return React.createElement(Square, { value: this.state.squares[i], click_back: function click_back() {
-          return _this2.handle_click(i);
+      return React.createElement(Square, { value: this.props.squares[i], click_back: function click_back() {
+          return _this2.props.click_back(i);
         } });
     }
   }, {
     key: "render",
     value: function render() {
-      var status = 'Next player: X';
+      var table = [];
+      var row = [];
+
+      for (var i = 1; i < 10; ++i) {
+        row.push(this.renderSquare(i - 1));
+        if (i % 3 == 0) {
+          var x = React.createElement(
+            "div",
+            { className: "board-row" },
+            row
+          );
+          table.push(x);
+          row = [];
+        }
+      }
 
       return React.createElement(
         "div",
         null,
-        React.createElement(
-          "div",
-          { className: "status" },
-          status
-        ),
-        React.createElement(
-          "div",
-          { className: "board-row" },
-          this.renderSquare(0),
-          this.renderSquare(1),
-          this.renderSquare(2)
-        ),
-        React.createElement(
-          "div",
-          { className: "board-row" },
-          this.renderSquare(3),
-          this.renderSquare(4),
-          this.renderSquare(5)
-        ),
-        React.createElement(
-          "div",
-          { className: "board-row" },
-          this.renderSquare(6),
-          this.renderSquare(7),
-          this.renderSquare(8)
-        )
+        table
       );
     }
   }]);
@@ -94,28 +89,113 @@ var Board = function (_React$Component) {
 var Game = function (_React$Component2) {
   _inherits(Game, _React$Component2);
 
-  function Game() {
+  function Game(props) {
     _classCallCheck(this, Game);
 
-    return _possibleConstructorReturn(this, (Game.__proto__ || Object.getPrototypeOf(Game)).apply(this, arguments));
+    var _this3 = _possibleConstructorReturn(this, (Game.__proto__ || Object.getPrototypeOf(Game)).call(this, props));
+
+    _this3.state = {
+      history: [{ squares: Array(9).fill(null) }],
+      step: 0,
+      x_next: true
+    };
+    return _this3;
   }
 
   _createClass(Game, [{
+    key: "jump_to",
+    value: function jump_to(step) {
+      this.setState({
+        step: step,
+        x_next: step % 2 === 0
+      });
+    }
+  }, {
+    key: "handle_click",
+    value: function handle_click(i) {
+      var history = this.state.history.slice(0, this.state.step + 1);
+      var current = history[history.length - 1];
+      var squares = current.squares.slice();
+
+      if (calculate_winner(squares) || squares[i]) return;
+
+      squares[i] = this.state.x_next ? 'X' : 'O';
+      this.setState({
+        history: history.concat([{ squares: squares }]),
+        step: history.length,
+        x_next: !this.state.x_next
+      });
+    }
+  }, {
+    key: "reset_board",
+    value: function reset_board() {
+      this.setState({
+        history: [{ squares: Array(9).fill(null) }],
+        step: 0,
+        x_next: true
+      });
+    }
+  }, {
     key: "render",
     value: function render() {
+      var _this4 = this;
+
+      var history = this.state.history;
+      var current = history[this.state.step];
+      var winner = calculate_winner(current);
+
+      var moves = history.map(function (step, move) {
+        var desc = move ? 'Go to move #' + move : 'Go to game start';
+        return React.createElement(
+          "li",
+          { key: move },
+          React.createElement(
+            "button",
+            { onClick: function onClick() {
+                return _this4.jump_to(move);
+              } },
+            desc
+          )
+        );
+      });
+
+      var status = void 0;
+      if (winner) {
+        status = "Winner: " + winner;
+      } else {
+        status = 'Next turn: ' + (this.state.x_next ? 'X' : 'O');
+      }
+
       return React.createElement(
         "div",
         { className: "game" },
         React.createElement(
           "div",
           { className: "game-board" },
-          React.createElement(Board, null)
+          React.createElement(Board, { squares: current.squares, click_back: function click_back(i) {
+              return _this4.handle_click(i);
+            } })
         ),
         React.createElement(
           "div",
           { className: "game-info" },
-          React.createElement("div", null),
-          React.createElement("ol", null)
+          React.createElement(
+            "div",
+            null,
+            status
+          ),
+          React.createElement(
+            "ol",
+            null,
+            moves
+          ),
+          React.createElement(
+            "button",
+            { className: "reset", onClick: function onClick() {
+                return _this4.reset_board();
+              } },
+            " Reset "
+          )
         )
       );
     }

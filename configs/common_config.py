b9121e8fb7fabbfa98149190125799b6b0b6886d
Braeden Mulligan <braeden.mulligan@gmail.com>
2023-12-31 14:06:02 -0800

    Restructure project files

--- configs/common_config.py
+++ configs/common_config.py
@@ -5,5 +5,5 @@ ENV = "development"
 if socket.gethostname() == "easyden-server":
 	ENV = "production"
 
-DEVICE_DEFINITIONS_PATH = "/../libraries/common/device_definition.h"
+DEVICE_DEFINITIONS_PATH = "/../devices/libraries/common/device_definition.h"
 DATABASE_PATH= "db/easyden.db"

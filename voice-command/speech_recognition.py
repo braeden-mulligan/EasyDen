732637b11adb09f3be2f1ba269edde4fb35de21d
Braeden Mulligan <braeden.mulligan@gmail.com>
2024-03-31 19:58:12 -0700

    Add voice command feedback tones and wake-word logic

--- voice-command/speech_recognition.py
+++ voice-command/speech_recognition.py
@@ -1,3 +1,4 @@
+import subprocess
 from vosk import Model, KaldiRecognizer
 
 import queue, sys, json, requests, os, logging, pyaudio
@@ -11,28 +12,36 @@ audio_data = queue.Queue()
 #TODO: configurable and force into lowercase.
 ASSISTANT_NAME = "edward"
 
-DEVICE_NAME = "TONOR TM20"
+AUDIO_INPUT_DEVICE = "TONOR TM20"
 SAMPLERATE = 48000
 
-def parse_command(transcription):
+AUDIO_OUTPUT_DEVICE = "hw:CARD=PCH,DEV=0"
+WAKE_TONE = ""
+CONFIRM_TONE = ""
+ERROR_TONE = ""
+
+def check_wake_word(transcription):
 	print(transcription)
 	if not transcription:
 		return
-
+	
 	words = transcription.lower().split()
 	
-	detected_device_type = None
-
-	if ASSISTANT_NAME not in words:
-		return
-
+	if ASSISTANT_NAME in words:
+		return True
+	
+	return False
+	
+def parse_command(transcription):
 	command_keywords = ["set", "switch", "switched", "turn", "change"]
+
+	words = transcription.lower().split()
 
 	for keyword in command_keywords:
 		if keyword in words:
 			break
 	else:
-		return
+		return False
 
 	specifiers = []
 	specifier_keywords = ["outlet", "socket", "outlets", "sockets"]
@@ -49,11 +58,9 @@ def parse_command(transcription):
 	if "on" in words:
 		setting = 1
 	elif "off" in words:
-		if setting:
-			return
 		setting = 0
 	else:
-		return
+		return False
 
 	device_fetch_url = "http://" + server_config.SERVER_ADDR + "/device/{}/refresh"
 	
@@ -64,6 +71,8 @@ def parse_command(transcription):
 	for d in devices:
 		if d["name"].lower() in transcription:
 			issue_command(d, specifiers, setting)
+
+	return True
 
 # Only handling poweroutlets for now.
 def issue_command(device, specifiers, setting):
@@ -109,13 +118,24 @@ def recognize_speech():
 		audio_data.put(bytes(data_in))
 		return (bytes(frames), pyaudio.paContinue)
 
-	istream = pa.open(input_device_index = find_mic_index(DEVICE_NAME), format = pyaudio.paInt16, channels = 1, rate = SAMPLERATE, input = True, frames_per_buffer = 1024, stream_callback = istream_callback, start = True)
+	istream = pa.open(input_device_index = find_mic_index(AUDIO_INPUT_DEVICE), format = pyaudio.paInt16, channels = 1, rate = SAMPLERATE, input = True, frames_per_buffer = 1024, stream_callback = istream_callback, start = True)
+
+	woke = False
 
 	while True:
 		if recog.AcceptWaveform(audio_data.get()):
-			result = json.loads(recog.Result())
-			parse_command(result["text"])
+			result = json.loads(recog.Result())["text"]
 
+			if woke:
+				if parse_command(result):
+					subprocess.Popen(["aplay", "-D", AUDIO_OUTPUT_DEVICE, CONFIRM_TONE])
+				else:
+					subprocess.Popen(["aplay", "-D", AUDIO_OUTPUT_DEVICE, ERROR_TONE])
+				woke = False
+
+			elif check_wake_word(result):
+				subprocess.Popen(["aplay", "-D", AUDIO_OUTPUT_DEVICE, WAKE_TONE])
+				woke = True
 
 def run():
 	log_dir = os.path.dirname(__file__) + "/logs"
